d9bcfeb300a68ab75ab63f1ae4450d71fef7471d
Arpit Bhardwaj <arpit.bhardwaj@sap.com>
2020-04-11 18:30:24 +0530

    ATM Machine design

---
+++ src/main/java/com/ab/design/atm/ATMMachine.java
@@ -0,0 +1,43 @@
+package com.ab.design.atm;
+
+/**
+ * @author Arpit Bhardwaj
+ */
+public class ATMMachine implements ATMMachineState{
+    private ATMMachineState machineState;
+
+    public ATMMachine() {
+        this.machineState = new NoDebitCardState();
+    }
+
+    public ATMMachineState getMachineState() {
+        return machineState;
+    }
+
+    public void setMachineState(ATMMachineState machineState) {
+        this.machineState = machineState;
+    }
+
+    @Override
+    public void insertDebitCard() {
+        this.machineState.insertDebitCard();
+        if (machineState instanceof NoDebitCardState){
+            ATMMachineState hasDebitCardState = new HasDebitCardState();
+            setMachineState(hasDebitCardState);
+        }
+    }
+
+    @Override
+    public void ejectDebitCard() {
+        machineState.ejectDebitCard();
+        if (machineState instanceof HasDebitCardState){
+            ATMMachineState noDebitCardState = new NoDebitCardState();
+            setMachineState(noDebitCardState);
+        }
+    }
+
+    @Override
+    public void enterPinAndWithdrawMoney() {
+        machineState.enterPinAndWithdrawMoney();
+    }
+}
