6cb82a86443f4c538207cc251c6a6251b6852446
Arpit Bhardwaj <arpitbhardwaj09@gmail.com>
2021-09-20 09:36:33 +0530

    Consistent Hashing

--- src/main/java/com/ab/design/consistenthashing/HashFunction.java
+++ src/main/java/com/ab/design/consistenthashing/HashFunction.java
@@ -3,13 +3,6 @@ package com.ab.design.consistenthashing;
 /**
  * @author Arpit Bhardwaj
  */
-public class HashFunction {
-    public int createHash(String input) {
-        int hash = 7;
-        for (int i = 0; i < input.length(); i++) {
-            hash = hash * 31 + input.charAt(i);
-        }
-
-        return hash;
-    }
+interface HashFunction {
+    long hash(String key);
 }
---
+++ src/main/java/com/ab/design/consistenthashing/Node.java
@@ -0,0 +1,11 @@
+package com.ab.design.consistenthashing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Arpit Bhardwaj
+ */
+interface Node {
+    String getKey();
+}
